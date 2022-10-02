7217b94e28821552977205e4a619483fd3af94e0
Hwang Seok O <ohs6006@gmail.com>
2022-10-02 20:27:40 +0900

    modal & firebase

---
+++ src/js/firebase.js
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCc9sS7BiLq7wZ8sNWahL1ow3-vNP6jGnA",
+  authDomain: "foodtruckarea.firebaseapp.com",
+  projectId: "foodtruckarea",
+  storageBucket: "foodtruckarea.appspot.com",
+  messagingSenderId: "689413924072",
+  appId: "1:689413924072:web:950ca41c8f14942def155d",
+  measurementId: "G-WTDYPKGMR0",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
+
+console.log(app);
---
+++ src/js/loginModal.js
@@ -0,0 +1,13 @@
+const openButton = document.getElementById("loginBtn");
+const modal = document.querySelector(".loginModal");
+const overlay = modal.querySelector(".overlay");
+const closeBtn = modal.querySelector("ion-icon");
+const openModal = () => {
+  modal.classList.remove("hidden");
+};
+const closeModal = () => {
+  modal.classList.add("hidden");
+};
+openButton.addEventListener("click", openModal);
+closeBtn.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
